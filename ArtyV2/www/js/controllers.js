217cc5a17848df54b160b45531917ed2f199c173
Jerald John Pormon <jeraldjohnpormon@yahoo.com>
2016-10-14 22:54:27 +0800

    Location on change

--- ArtyV2/www/js/controllers.js
+++ ArtyV2/www/js/controllers.js
@@ -1,35 +1,4 @@
 var app = angular.module('app.controllers', [])
-.controller('collaborationCtrl', ['$scope', '$stateParams', '$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicModal) {
-	$ionicModal.fromTemplateUrl('templates/modal-add.html', {
-	   scope: $scope,
-	   animation: 'slide-in-up'
-	 }).then(function(modal) {
-	   $scope.modal = modal;
-	 });
-	 $scope.openModal = function() {
-	   $scope.modal.show();
-	 };
-	 $scope.closeModal = function() {
-	   $scope.modal.hide();
-	 };
-	 // Cleanup the modal when we're done with it!
-	 $scope.$on('$destroy', function() {
-	   $scope.modal.remove();
-	 });
-	 // Execute action on hide modal
-	 $scope.$on('modal.hidden', function() {
-	   // Execute action
-	 });
-	 // Execute action on remove modal
-	 $scope.$on('modal.removed', function() {
-	   // Execute action
-	 });
-
-}])
-   
 .controller('requestsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
