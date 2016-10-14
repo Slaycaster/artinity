97e675f71e0fcaef9bb9ff90fc0595828230b9e9
killandibuz <killandibuz@gmail.com>
2016-10-14 20:39:32 +0800

    Controllers

--- ArtyV2/www/js/controllers.js
+++ ArtyV2/www/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', ['$scope', '$stateParams', '$ionicModal',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$ionicModal', '$ionicPopup',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicModal) {
+function ($scope, $stateParams, $ionicModal, $ionicPopup) {
 	$ionicModal.fromTemplateUrl('templates/modal-location.html', {
 	   scope: $scope,
 	   animation: 'slide-in-up'
@@ -29,6 +29,35 @@ function ($scope, $stateParams, $ionicModal) {
 	   // Execute action
 	 });
 
+	 $scope.showPopup = function(index){
+	 	$scope.data = {};
+	 	var myPopup = $ionicPopup.show({
+	     template: '<input type="text" ng-model="data.wifi">',
+	     title: 'Request for collab',
+	     subTitle: 'Send a short message. :)',
+	     scope: $scope,
+	     buttons: [
+	       { text: 'Cancel' },
+	       {
+	         text: '<b>Send</b>',
+	         type: 'button-balanced',
+	         onTap: function(e) {
+	           if (!$scope.data.wifi) {
+	             //don't allow the user to close unless he enters wifi password
+	             e.preventDefault();
+	           } else {
+	             return $scope.data.wifi;
+	           }
+	         }
+	       }
+	     ]
+	   });
+
+	   myPopup.then(function(res) {
+	     console.log('Tapped!', res);
+	     console.log(index);
+	   });
+	 };	 
 }])
    
 .controller('collaborationCtrl', ['$scope', '$stateParams', '$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
